b33b9ad8b07d95305ea5249770c87fe339ca6b91
Philipp Gl <Philipp Gl>
2016-06-03 12:48:43 +0200

    first comments

--- src/at/fhj/itm/Point2d.java
+++ src/at/fhj/itm/Point2d.java
@@ -1,24 +1,54 @@
 package at.fhj.itm;
+
+/**
+ * 
+ * @author Philipp Glanzer
+ * Creates a 2d point 
+ *
+ */
 class Point2d {
 	protected boolean debug;
 	private double x;
 	private double y;
 
+	/**
+	 * Constructor.
+	 * initalize a point with px and py
+	 * @param px x coordinate of the point
+	 * @param py y coordinate of the point
+	 */
 	public Point2d (double px, double py)
 	{ 
 		this.x = px;
 		this.y  = py;
 	}
 
+	/**
+	 * Constructor.
+	 * initalize a point with 0/0s
+	 */
 	public Point2d () 
 	{		
-		// just create a point of 0/0
+		this.x = 0;
+		this.y = 0;
 	}
 
+	/**
+	 * Constructor.
+	 * initalize point with
+	 * @param pt x coordinate and y coordinate
+	 */
 	public Point2d (Point2d pt) 
 	{	
+		this.x = pt.x;
+		this.y = pt.y;
 	}
 
+	/**
+	 * Prints debug: s
+	 * if debug is true
+	 * @param s String 
+	 */
 	public void dprint (String s) {
 		if (debug)
 			System.out.println("Debug: " + s);
@@ -30,6 +60,7 @@ class Point2d {
 
 	public void setX(double px) {
 		dprint ("setX(): Changing value of X from " + x + " to " + px );
+		this.x = px;
 	}
 
 	public double getX() {
@@ -38,6 +69,7 @@ class Point2d {
 
 	public void setY(double py)  {
 		dprint ("setY(): Changing value of Y from " + y + " to " + py );
+		this.y = py;
 	}
 
 	public double getY() {
--- test/at/fhj/itm/Point2dTest.java
+++ test/at/fhj/itm/Point2dTest.java
@@ -1,5 +1,72 @@
 package at.fhj.itm;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Point2dTest {
 
+	private Point2d p2d;
+	
+	@Before
+	public void setUp(){
+		p2d = new Point2d();
+	}
+
+	/**
+	 * Test the Setter of the x-coordinate
+	 * - set the coordinate
+	 * - get the value from the object
+	 * - try to compare your value with the return value of x
+	 */
+	@Test
+	public void testSetX(){
+		p2d.setX(10);
+		assertEquals(10, p2d.getX(), 0.0001);
+	}
+
+	/**
+	 * Test the Setter of the y-coordinate
+	 * - set the coordinate
+	 * - get the value from the object
+	 * - try to compare your value with the return value of y
+	 */
+	@Test
+	public void testSetY(){
+		p2d.setY(10);
+		assertEquals(10, p2d.getY(), 0.0001);
+		
+	}
+	
+
+	/**
+	 * Test the distanceFrom() Method
+	 * - call the method distanceFrom and calculate the distance
+	 * - calculate the distance by your own implementation
+	 * - try to compare your result with the return value of distanceFrom()
+	 */
+	@Test
+	public void testDistanceFrom(){
+		p2d.setX(5);
+		p2d.setY(4);
+		
+		Point2d p2dtest = new Point2d(5, 4);
+		
+		assertEquals(0, p2d.distanceFrom(p2dtest), 0.00001);
+
+	}
+
+	/**
+	 * Test the distanceFromOrigin() Method
+	 * - call the method distanceFromOrigin and calculate the distance
+	 * - calculate the distance by your own implementation
+	 * - try to compare your result with the return value of distanceFromOrigin()
+	 */
+	@Test
+	public void testDistanceFromOrigin(){	
+		p2d.setX(4);
+		p2d.setY(3);
+		
+		assertEquals(5, p2d.distanceFromOrigin(), 0.0001);
+	}
 }
